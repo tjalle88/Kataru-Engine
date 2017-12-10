e7f21cd13ab8cc19f954c0238f81bb19671be80a
Kalle Takeuchi <tjalle88@gmail.com>
2017-12-10 14:45:52 +0100

    Initial Commit

---
+++ Kataru EngineTests/Kataru_EngineTests.swift
@@ -0,0 +1,36 @@
+//
+//  Kataru_EngineTests.swift
+//  Kataru EngineTests
+//
+//  Created by Kalle Jönsson on 2017-12-10.
+//  Copyright © 2017 Takemaki Studios. All rights reserved.
+//
+
+import XCTest
+@testable import Kataru_Engine
+
+class Kataru_EngineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
